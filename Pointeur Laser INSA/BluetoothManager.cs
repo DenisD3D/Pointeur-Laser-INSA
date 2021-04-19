024c22894f1428c85d3a11860b187c13dc60d53c
DenisD3D <dd.denis2544@gmail.com>
2021-04-19 11:29:54 +0200

    Create basic console windows

---
+++ Pointeur Laser INSA/BluetoothManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Pointeur_Laser_INSA
+{
+    class BluetoothManager
+    {
+        public string port;
+        private readonly Dispatcher dispatcher;
+        public SerialPort _serialPort;
+        public bool _continue;
+        private Thread readThread;
+        private Action<string> onData;
+        private Action<string> onError;
+
+        public BluetoothManager(string port, System.Windows.Threading.Dispatcher dispatcher, Action<string> onData, Action<string> onError)
+        {
+            this.port = port;
+            this.dispatcher = dispatcher;
+            this.onData = onData;
+            this.onError = onError;
+            readThread = new Thread(Read);
+
+            try
+            {
+                _serialPort = new SerialPort(port, 9600);
+                _serialPort.ReadTimeout = 500;
+                _serialPort.WriteTimeout = 500;
+                _serialPort.Open();
+
+                _continue = true;
+                readThread.Start();
+            }
+            catch
+            {
+                _continue = false;
+                dispatcher.Invoke(onError, "Can't connect to the specified port");
+            }
+
+        }
+
+        public void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    if (_serialPort.BytesToRead > 0)
+                    {
+                        string message = _serialPort.ReadLine();
+                        dispatcher.Invoke(onData, message);
+                    }
+                }
+                catch (TimeoutException) { }
+            }
+        }
+
+        public static string[] GetComsPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public void Write(string s)
+        {
+            try
+            {
+                _serialPort.Write(s);
+            }
+            catch
+            {
+                _continue = false;
+                dispatcher.Invoke(onError, "Connection timed out");
+            }
+        }
+
+        public void close()
+        {
+            _continue = false;
+            readThread.Join();
+            _serialPort.Close();
+        }
+    }
+}
--- Pointeur Laser INSA/MainWindow.xaml.cs
+++ Pointeur Laser INSA/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Pointeur_Laser_INSA
 {
@@ -20,14 +9,54 @@ namespace Pointeur_Laser_INSA
     /// </summary>
     public partial class MainWindow : Window
     {
+        BluetoothManager bluetoothManager;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
+            label1.Content = "Available Com port : ";
+            foreach (string port in BluetoothManager.GetComsPorts())
+            {
+                label1.Content += port + " ";
+            }
+            label1.Content += "\n";
+        }
 
+        public void onData(string message)
+        {
+            if (message.StartsWith("ILP+JOYSTICK="))
+                return;
+            label1.Content += message + "\n";
+        }
+
+        public void onError(string message)
+        {
+            label1.Content += message + "\n";
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            bluetoothManager.close();
+        }
+
+        private void textbox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (bluetoothManager == null || !bluetoothManager._continue || !bluetoothManager._serialPort.IsOpen)
+                {
+                    label1.Content += "Connection to " + textbox1.Text + "\n";
+                    bluetoothManager = new BluetoothManager(textbox1.Text, Dispatcher, onData, onError);
+                }
+                else {
+                    bluetoothManager.Write(textbox1.Text);
+                }
+                textbox1.Text = "";
+            }
         }
     }
 }
